4b2c2498ba5f2c2e02943e969b0576fe94720a49
Miss Nuriniso <nurinisosultonmurodova@gmail.com>
2025-05-07 15:40:37 +0300

    admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+# COUNTRY / TRIP
+admin.site.register(models.Country)
+admin.site.register(models.Locations)
+admin.site.register(models.LocationImage)
+admin.site.register(models.Trip)
+admin.site.register(models.TripDays)
+admin.site.register(models.Service)
+admin.site.register(models.TripImages)
+admin.site.register(models.TripOrder)
+
+
