b1a81578a5045e9f36f458b8b9c90db5832fdd97
Miss Nuriniso <nurinisosultonmurodova@gmail.com>
2025-05-09 15:47:47 +0300

    country va trips crud

--- dashboard/Country/urls.py
+++ dashboard/Country/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #Country / Trip start
-
     path('countries/list/', views.country_list, name='country_list'),
     path('country/detail/<str:uuid>/', views.country_detail, name='country_detail'),
-
-    #Country / Trip end
+    path('country/edit/<str:uuid>/', views.country_edit, name='country_edit'),
+    path('country/delete/<str:uuid>/', views.country_delete, name='country_delete'),
+    path('country/create/', views.country_create, name='country_create')
 ]
